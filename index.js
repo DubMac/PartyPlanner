cca6440e860eaec103e0737c4b26d1d520ecf154
dub-2k <135155561+dub-2k@users.noreply.github.com>
2023-11-16 05:31:15 -0800

    final version

---
+++ index.js
@@ -0,0 +1,117 @@
+
+const COHORT = "2308-ACC-PT-WEB-PT";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+const state = {
+  events: [],
+};
+
+//Refence constants for HTML fields
+const eventList = document.querySelector("#events");
+const addEventForm = document.querySelector("#addEvent");
+addEventForm.addEventListener("submit", addEvent);
+
+ //Sync state with async and render
+async function render() {
+  await getEvents();
+  renderEvents();
+}
+render();
+
+//Get events information from API
+async function getEvents() {
+  try {
+    const response = await fetch(API_URL);
+   if (!response.ok) {
+    throw new Error("Failed to fetch events");
+   }
+    
+    const data = await response.json();
+    state.events = data.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+//Render events in list format with CSS formatting, also includes addition of delete button in list
+function renderEvents() {
+  if (!state.events.length) {
+    eventList.innerHTML = "<li>No events.</li>";
+    return;
+  }
+  const eventCards = state.events.map((event) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <h2>${event.name}</h2>
+      <p>${event.description}</p>
+      <p>${event.location}</p>
+      <p>${event.date}</p>
+      <button class="delete-button" data-event-id="${event.id}">Delete</button>
+    `;
+    return li;
+  });
+  eventList.replaceChildren(...eventCards);
+}
+
+//Event listener for delete button
+eventList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-button")) {
+      const eventId = event.target.getAttribute("data-event-id");
+      deleteEvent(eventId);
+    }
+  });
+/**
+ * Ask the API to create a new event based on form data
+ * @param {Event} event
+ */
+
+
+
+
+async function addEvent(event) {
+  event.preventDefault();
+  try {
+     // Convert html date input format to object, then convert to ISO Date format 
+     const dateInputValue = new Date(addEventForm.date.value);
+
+     if (isNaN(dateInputValue)) {
+       throw new Error("Invalid date format");
+     }
+
+     const isoDateString = dateInputValue.toISOString();
+// addEvent function to POST event input
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: addEventForm.name.value,
+        description: addEventForm.description.value,
+        location: addEventForm.location.value,
+        date: isoDateString,
+        //date: addEventForm.date.value, (this was what I originally had but the date format errored out the update)
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create event");
+    }
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
+//Delete function for event API data
+async function deleteEvent(eventId) {
+    try {
+      const response = await fetch(API_URL + `/${eventId}`, {
+        method: "DELETE",
+      });
+      
+      if (response.ok) {
+        // Event successfully deleted, update the UI or throw error if failed
+        render();
+      } else {
+        throw new Error("Failed to delete event");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
